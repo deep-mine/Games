00f40a52cab07f5ebfa3db6329b2e625baa5b912
Dibyanshu Bhoi <dibyanshu.bhoi@gmail.com>
2017-09-09 19:10:01 +0530

    snake-you vs blue files added

---
+++ snake-you_vs_blue/aisnake.js
@@ -0,0 +1,116 @@
+function AiSnake(fx,fy){
+
+	this.snake_x = 200;
+	this.snake_y = 200;
+	this.x_speed = 0;
+	this.y_speed = 0;
+	this.snake_speed = 20;
+	this.sq_size = 20;
+	
+	this.tail = [];
+	this.snake_length = 0;
+	
+	var food_x = fx;
+	var food_y = fy;
+	
+	var dx1,dx2,dy1,dy2 = 0;
+	var xDir,yDir = 0;
+	
+	this.move = function(food_x,food_y){
+		
+		dx1 = abs(food_x - this.snake_x);
+		dx2 = abs(this.snake_x + width - food_x);
+		
+		dy1 = abs(food_y - this.snake_y);
+		dy2 = abs(this.snake_y + height - food_y);
+		
+		if(food_x > this.snake_x){
+			if(dx1<=dx2){
+				xDir = this.snake_speed;
+				yDir = 0;
+			}
+			else{
+				xDir = -1*this.snake_speed;
+				yDir = 0;
+			}
+		}
+		if(food_x < this.snake_x){
+			if(dx1<=dx2){
+				xDir = -1*this.snake_speed;
+				yDir = 0;
+			}
+			else{
+				xDir = this.snake_speed;
+				yDir = 0;
+			}
+		}
+		
+		if(food_x == this.snake_x && food_y < this.snake_y){
+			if(dy1<=dy2){
+				yDir = -1*this.snake_speed;
+				xDir = 0;
+			}
+			else{
+				yDir = this.snake_speed;
+				xDir = 0;
+			}
+		}
+		
+		if(food_x == this.snake_x && food_y > this.snake_y){
+			if(dy1<=dy2){
+				yDir = this.snake_speed;
+				xDir = 0;
+			}
+			else{
+				yDir = -1*this.snake_speed;
+				xDir = 0;
+			}
+		}
+		
+		this.x_speed = xDir;
+		this.y_speed = yDir;
+	}
+	
+	this.update = function(){
+		if(this.snake_length == this.tail.length){
+			for(var i = 0;i<this.snake_length-1;i++){
+				this.tail[i] = this.tail[i+1];
+			}
+		}
+		this.tail[this.snake_length-1] = createVector(this.snake_x,this.snake_y);
+	
+		this.snake_x += this.x_speed;
+		this.snake_y += this.y_speed;
+	
+		if(this.snake_x < 0){
+			this.snake_x = width-this.sq_size;
+		}
+	
+		if(this.snake_x > width-this.sq_size){
+			this.snake_x = 0;
+		}
+	
+		if(this.snake_y < 0){
+			this.snake_y = height-this.sq_size;
+		}
+	
+		if(this.snake_y > height-this.sq_size){
+			this.snake_y = 0;
+		}
+	}
+	
+	
+	this.show = function(){
+		
+		fill(color(100,100,255));
+		noStroke();
+		for(var i = 0;i<this.snake_length;i++){
+			rect(this.tail[i].x,this.tail[i].y,this.sq_size,this.sq_size);
+		}
+	
+		fill(color(100,100,255));
+		noStroke();
+		rect(this.snake_x,this.snake_y,this.sq_size,this.sq_size);
+	}
+		
+}
